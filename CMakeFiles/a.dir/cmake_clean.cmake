4d45e793622b987952227ac43ad8af08d315394e
shuerrr <729375337@qq.com>
2022-10-16 02:18:54 +0800

    123

---
+++ CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/BigFloat.cc.o"
+  "CMakeFiles/a.dir/BigFloat.cc.o.d"
+  "CMakeFiles/a.dir/main.cpp.o"
+  "CMakeFiles/a.dir/main.cpp.o.d"
+  "a"
+  "a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
